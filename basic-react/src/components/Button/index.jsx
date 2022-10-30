ab5ad1f6ec9678ae28c73a19c3045c5ab84a724e
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-10-29 22:03:24 -0300

    Creating css to each component

--- basic-react/src/components/Button/index.jsx
+++ basic-react/src/components/Button/index.jsx
@@ -1,6 +1,9 @@
+import './styles.css'
+
 export const Button = ({ text, onClick }) => {
     return (
         <button
+            className='load-more'
             type='button'
             onClick={onClick}
         >
--- basic-react/src/components/UserCard/index.jsx
+++ basic-react/src/components/UserCard/index.jsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 export const UserCard = ({ user }) => {
     return (
         <div className='user'>
