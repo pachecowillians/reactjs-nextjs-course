0a35f7dff62ed9c6ca76698d20fcce35a1714340
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-11-01 09:54:42 -0300

    Converting class component to functional component

--- basic-react/src/templates/Home/index.jsx
+++ basic-react/src/templates/Home/index.jsx
@@ -1,116 +1,84 @@
 import './styles.css';
-import { Component } from 'react';
+import { Component, useCallback, useEffect, useState } from 'react';
 import { UserCard } from '../../components/UserCard';
 import { loadUsers } from '../../utils/loadUsers';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
-export class Home extends Component {
+export const Home = () => {
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(0);
+    const [usersPerPage, setUsersPerPage] = useState(4);
+    const [allUsers, setAllUsers] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
-    constructor(props) {
-        super(props);
+    const noMoreUsers = page + usersPerPage >= allUsers.length;
 
-        this.state = {
-            users: [],
-            allUsers: [],
-            page: 0,
-            usersPerPage: 4,
-            searchValue: ''
-        }
-    }
+    const filteredUsers = !!searchValue ?
+        allUsers.filter(user => user.name.toLowerCase().includes(searchValue.toLowerCase()))
+        :
+        users;
 
-    componentDidMount() {
-        this.fillUsers();
-        this.setState({
-            name: 'Willian Pacheco',
-            counter: 0
-        })
-    }
-
-    fillUsers = async () => {
-        const { page, usersPerPage } = this.state;
+    const fillUsers = useCallback(async (page, usersPerPage) => {
         const usersAndPhotos = await loadUsers();
 
-        this.setState({
-            users: usersAndPhotos.slice(page, usersPerPage),
-            allUsers: usersAndPhotos,
-        });
-    }
+        setUsers(usersAndPhotos.slice(page, usersPerPage));
+        setAllUsers(usersAndPhotos);
+    }, []);
 
-    loadMorePosts = () => {
-        const { page, usersPerPage, allUsers, users } = this.state;
+    useEffect(() => {
+        fillUsers(0, usersPerPage);
+    }, [fillUsers, usersPerPage]);
 
+    const loadMoreUsers = () => {
         const nextPage = page + usersPerPage;
         const nextUsers = allUsers.slice(nextPage, nextPage + usersPerPage);
+
         users.push(...nextUsers);
-        this.setState({
-            users: users,
-            page: nextPage
-        })
+
+        setUsers(users);
+        setPage(nextPage);
     }
 
-    componentDidUpdate() {
-        // console.log("The component has been updated!");
-    }
-
-    componentWillUnmount() {
-        // console.log("The component will unmount");
-    }
-
-    handleCounterClick = () => {
-        this.setState({ counter: this.state.counter + 1 });
-    }
-
-    handleChange = (e) => {
+    const handleChange = (e) => {
         const { value } = e.target;
-        this.setState({ searchValue: value });
+        setSearchValue(value);
     }
 
-    render() {
+    return (
+        <div className='container'>
+            {
+                !!searchValue && (
+                    <h1>Search value: {searchValue}</h1>
+                )
+            }
+            <TextInput value={searchValue} onChange={handleChange} />
 
-        const { users, page, usersPerPage, allUsers, searchValue } = this.state;
-        const noMoreUsers = page + usersPerPage >= allUsers.length;
-
-        const filteredUsers = !!searchValue ?
-            allUsers.filter(user => user.name.toLowerCase().includes(searchValue.toLowerCase()))
-            :
-            users;
-
-        return (
-            <div className='container'>
-                {
-                    !!searchValue && (
-                        <h1>Search value: {searchValue}</h1>
-                    )
-                }
-                <TextInput value={searchValue} onChange={this.handleChange} />
-
-                {filteredUsers.length > 0 ?
-                    (
-                        <div className='users'>
-                            {
-                                filteredUsers.map(
-                                    user => (
-                                        <UserCard key={user.id} user={user} />
-                                    )
+            {filteredUsers.length > 0 ?
+                (
+                    <div className='users'>
+                        {
+                            filteredUsers.map(
+                                user => (
+                                    <UserCard key={user.id} user={user} />
                                 )
-                            }
+                            )
+                        }
 
-                        </div>
-                    )
-                    :
-                    (
-                        <h3>No users found!</h3>
-                    )}
-
-                {!searchValue && (
-                    <Button
-                        text="Load more"
-                        disabled={noMoreUsers}
-                        onClick={this.loadMorePosts}
-                    />
+                    </div>
+                )
+                :
+                (
+                    <h3>No users found!</h3>
                 )}
-            </div>
-        );
-    }
+
+            {!searchValue && (
+                <Button
+                    text="Load more"
+                    disabled={noMoreUsers}
+                    onClick={loadMoreUsers}
+                />
+            )}
+        </div>
+    );
 }
