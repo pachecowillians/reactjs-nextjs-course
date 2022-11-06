a25ee95e48d0e1cfb1d71909518853cfa8b64508
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-11-05 22:00:41 -0300

    Configuring eslint and Prettier

---
+++ react-hooks/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+};
