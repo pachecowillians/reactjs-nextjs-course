be7b7bef912ad4dd5d49ecbdecbb7a2c6c439da4
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-11-16 15:32:56 -0300

    Using useRef

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,7 +1,7 @@
 import propTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 
 const Button = ({ handleClick }) => {
     console.log('Son rendered');
@@ -18,6 +18,8 @@ Button.propTypes = {
 
 function App() {
     const [reverse, setReverse] = useState(false);
+
+    const image = useRef(null);
 
     // useEffect(() => {
     //     console.log('componentDidUpdate');
@@ -39,14 +41,19 @@ function App() {
 
     console.log('Father rendered');
 
+    const handleImageClick = () => {
+        console.log(image.current.src);
+    };
+
     const handleClick = useCallback(() => {
         setReverse((prevReverse) => !prevReverse);
     }, []);
 
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header" onClick={handleImageClick}>
                 <img
+                    ref={image}
                     src={logo}
                     className={`App-logo${reverse ? '-reverse' : ''}`}
                     alt="logo"
