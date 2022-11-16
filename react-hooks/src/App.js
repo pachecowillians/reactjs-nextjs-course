70eb439b6730e1273c1fe5c96da2a09722df512f
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-11-16 09:32:17 -0300

    using useEffect to show react lifeCycle methods

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
     const [reverse, setReverse] = useState(false);
+
+    useEffect(() => {
+        console.log('componentDidUpdate');
+    });
+
+    useEffect(() => {
+        console.log('componentDidMount');
+    }, []);
+
+    useEffect(() => {
+        console.log('reverse updated to: ', reverse);
+    }, [reverse]);
+
+    useEffect(() => {
+        return () => {
+            console.log('componentWillUmount');
+        };
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
