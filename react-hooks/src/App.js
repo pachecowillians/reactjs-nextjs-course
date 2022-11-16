9649f57a8353406f33acf0ed9155519c084a377a
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-11-16 08:59:03 -0300

    using useState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,57 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { useState } from 'react';
 
-// function App() {
-//     return (
-//         <div className="App">
-//             <header className="App-header">
-//                 <img src={logo} className="App-logo" alt="logo" />
-//                 <p>
-//                     Edit <code>src/App.js</code> and save to reload.
-//                 </p>
-//                 <a
-//                     className="App-link"
-//                     href="https://reactjs.org"
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                 >
-//                     Learn React
-//                 </a>
-//             </header>
-//         </div>
-//     );
-// }
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            reverse: false,
-        };
-    }
-    render() {
-        const { reverse } = this.state;
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img
-                        src={logo}
-                        className={`App-logo${reverse ? '-reverse' : ''}`}
-                        alt="logo"
-                    />
-                    <button
-                        type="button"
-                        onClick={() => {
-                            this.setState({ reverse: !reverse });
-                        }}
-                    >
-                        Reverse
-                    </button>
-                </header>
-            </div>
-        );
-    }
+function App() {
+    const [reverse, setReverse] = useState(false);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={logo}
+                    className={`App-logo${reverse ? '-reverse' : ''}`}
+                    alt="logo"
+                />
+                <button
+                    type="button"
+                    onClick={() => {
+                        setReverse(!reverse);
+                    }}
+                >
+                    Reverse
+                </button>
+            </header>
+        </div>
+    );
 }
+
 export default App;
