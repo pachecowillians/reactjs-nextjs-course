b5d12a1f80ad2e14aeeedd76979ec356a67d2b20
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-11-17 09:15:41 -0300

    Using useReducer

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import React, { useContext, useMemo, useRef } from 'react';
 import { Button } from './components/MyButton';
 import { GlobalContext } from './contexts/AppContext';
+import { useReducer } from 'react';
 
 function App() {
     const context = useContext(GlobalContext);
 
     const image = useRef(null);
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'changeName':
+                return { ...state, title: 'WPS - Changed' };
+            case 'hoverName':
+                return { ...state, title: 'WPS - Hovered' };
+            default:
+                break;
+        }
+        return { ...state };
+    };
+
+    const [state, dispatch] = useReducer(reducer, { title: 'WPS' });
 
     return (
         <div className="App">
@@ -20,6 +35,12 @@ function App() {
                     }`}
                     alt="logo"
                 />
+                <h1
+                    onClick={() => dispatch({ type: 'changeName' })}
+                    onMouseOver={() => dispatch({ type: 'hoverName' })}
+                >
+                    {state.title}
+                </h1>
                 {useMemo(
                     () => (
                         <Button />
